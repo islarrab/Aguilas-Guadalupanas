65494defe13337785f95b6d10c52dce9fea75976
Victor Nava <viko.nava@gmail.com>
2011-01-24 19:05:40 -0800

    Initial Website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VikoBlog::Application.config.secret_token = '11f0e941ae4f872b122920252e7de9c739f37bb1f87ed208427f1dab947e73c636caecadd0ec123d3b8b14f8ca40fbd080f3fab500910a8ed75ade10e5f7497d'
