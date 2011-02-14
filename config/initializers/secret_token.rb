65555705502cca5cd366c7564e477cddfe562da1
Isaac Larraguibel <islarrab@gmail.com>
2011-02-14 15:38:36 -0600

    La base Ruby, con heroku, devise y cancan ya instalados

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AguilasGuadalupanas::Application.config.secret_token = '6176bb28321e449accac5d54e4fd46c7a191d550ccf7ddef68c6752abfe5acf3c63304aefd77914d1fcae1f3ee282d26f73060b7e3695a259fd4786015d63b61'
