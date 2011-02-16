7cb8c7c0260b83c29e916727d83ee38d47953abe
Viko Nava <viko.nava@gmail.com>
2011-02-15 21:13:06 -0800

    Default template + Usernames + Devise funcionando

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,20 @@ class User < ActiveRecord::Base
   :username, :nombre, :apellido_paterno, :apellido_materno, :horas_ssc, :tec,
   :carrera, :matricula, :semestre, :telefono, :celular, :direccion
 
+  validates_uniqueness_of :username
+  validates_uniqueness_of :email
   validates_confirmation_of :password
   validates_format_of :email, :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   validates_presence_of :password_confirmation, :username, :nombre,
   :apellido_paterno, :apellido_materno, :tec, :carrera, :matricula,
-  :direccion
+  :direccion, :semestre
   validates_length_of :password, :in => 6..20
   validates_length_of :telefono, :in => 8..10, :allow_nil => false
   validates_length_of :celular, :is => 10, :allow_blank => true
+  validates_numericality_of :horas_ssc, :only_integer => true
+  validates_numericality_of :matricula, :only_integer => true
+  validates_numericality_of :semestre, :only_integer => true
+  validates_numericality_of :telefono, :only_integer => true
+  validates_numericality_of :celular, :only_integer => true
+
 end
